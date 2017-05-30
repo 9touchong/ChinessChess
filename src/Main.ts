f3e45fcb6ed247eb2cb018c23cc38bb03cbdf9bb
9touchong <jackal12@qq.com>
2017-05-30 20:23:50 +0800

    170530

--- src/Main.ts
+++ src/Main.ts
@@ -6,6 +6,8 @@ class Main extends egret.DisplayObject {
 	protected _egret3DCanvas: egret3d.Egret3DCanvas;
 	// View3D操作对象
 	protected _view3D: egret3d.View3D;
+	//一个立方体
+	//protected cube2: egret3d.Mesh;
 	// 当前的相机对象
 	protected _camera: egret3d.Camera3D;
 	// 当前按键状态
@@ -54,8 +56,14 @@ class Main extends egret.DisplayObject {
 		var geometery_Cube: egret3d.CubeGeometry = new egret3d.CubeGeometry();
 		//通过材质球和geometery数据创建一个mesh对象
 		var cube = new egret3d.Mesh(geometery_Cube, mat_cube);
+		var cube2 = new egret3d.Mesh(new egret3d.CubeGeometry(),new egret3d.TextureMaterial());
 		//将mesh节点添加到View3D内
 		this._view3D.addChild3D(cube);
+		cube2.x+=100;
+		cube2.z+=100;
+		this._view3D.addChild3D(cube2);
+		cube2.enablePick = true;
+		cube2.addEventListener(egret3d.PickEvent3D.PICK_CLICK, this.OnPickClick, this);
 		///创建面片，放置于场景内(0,0,0)位置
 		///创建一个默认的贴图材质球
 		var mat_Plane: egret3d.TextureMaterial = new egret3d.TextureMaterial();
@@ -63,6 +71,7 @@ class Main extends egret.DisplayObject {
 		var geometery_Plane: egret3d.PlaneGeometry = new egret3d.PlaneGeometry();
 		//通过材质球和geometery数据创建一个mesh对象
 		var plane = new egret3d.Mesh(geometery_Plane, mat_Plane);
+		plane.addEventListener(egret3d.MouseEvent3D.MOUSE_CLICK,this.clickBoard,this);
 		//将mesh节点添加到View3D内
 		this._view3D.addChild3D(plane);
 		///设置默认值-1
@@ -74,6 +83,9 @@ class Main extends egret.DisplayObject {
 		///注册鼠标回弹事件
 		egret3d.Input.addEventListener(egret3d.KeyEvent3D.KEY_UP, this.OnKeyUp, this);
 		console.log("Hello World,Hello Egret3D");
+		console.log("cube",cube.x,cube.y,cube.z);
+		console.log("cube1",cube2.x,cube2.y,cube2.z);
+		console.log("plane",plane.x,plane.y,plane.z);
 	}
 	//鼠标回弹事件
 	public OnKeyUp(e: egret3d.KeyEvent3D) {
@@ -111,5 +123,17 @@ class Main extends egret.DisplayObject {
 				this._camera.x += -1;
 				break;
 		}
+	}
+	//自定义鼠标事件
+	///点选棋子
+	public OnPickClick(e: egret3d.Event3D){
+        ///pick的世界坐标
+        var pos = e.target.globalPosition;
+        ///这里我们将信息输出
+		console.log("you have picked me","pos:" + pos.x + "," + pos.y + "," + pos.z);
+	}
+	///点击棋盘
+	public clickBoard(e: egret3d.Event3D){
+		console.log ("clicked the chess board");
 	}
 }
