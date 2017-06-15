8fb175534f0abed9664dd5d9abcb1f3cfbd33b48
9touchong <jackal12@qq.com>
2017-06-15 10:50:15 +0800

    n

--- src/BLL/logicpiece.ts
+++ src/BLL/logicpiece.ts
@@ -8,7 +8,8 @@ class LogicPiece extends Object{
 	private p_role : string;
 	private p_faction : string;
 	private p_id : string;
-    private Landing_points; //棋子下一步可能落点集
+    private landing_points; //棋子下一步可能落点集，数组，元素是[m_x,m_y]
+    private menace_pieces;  //棋子可以威胁到的敌方子，即下一步可以吃到的子,数组,元素是p_id
     public constructor(p_role:string,p_faction:string,m_x:number,m_y:number,p_id?:string){
         super();
         if (p_id){
@@ -22,20 +23,25 @@ class LogicPiece extends Object{
     public set_p_id(p_id:string){
 		this.p_id = p_id;
 	}
-    public get_LandingPoints(map){
+    public effect_update(map,p_set){
         /**
-         * 获得当前棋子的所有合法着点
-         * 因为这个的实现要一个完整的map，所以放在上一级logicplay中也可以，好像还更合理，以后也可能转移
-         * 参数map是一个9*10基本元素是[role,faction]的二维数组
+         * 影响力更新
+         * 获得当前棋子的所有合法着点和可直接吃掉的子，这些可以看做棋子在棋局中的影响力
+         * 因为这个的实现要一个完整的map和所有棋子对照表p_set，所以放在上一级logicplay中也可以，好像还更合理，以后也可能转移
+         * 参数map和p_set就是LogicPlay中的Map和pieces_set；
+         * 不设返回值，只是更新当前棋子的landing_points和menace_pieces属性
          */
-        let tem_points = [];
-        let [Min_x,Min_y,Max_x,Max_y] = [0,0,9,10]; //map的最大范围，横9纵10，这样声明一下主要便于以后修改
+        var tem_points = [];
+        var tem_pieces = [];
+        var [Min_x,Min_y,Max_x,Max_y] = [0,0,8,9]; //map的最大范围，横9纵10，这样声明一下主要便于以后修改
         switch(this.p_role){
             case "c":
+                for (let t_x=this.m_x ; t_x>=Min_x ; t_x--){}
                 break;
             default:
                 tem_points = null;
         }
-        this.Landing_points = tem_points;
+        this.landing_points = tem_points;
+        this.menace_pieces = tem_pieces;
     }
 }
